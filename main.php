34ab009fff82077aa8627510c10f89b234dc8662
Mohamed Ashraf <109896315+mohamed-ashraf-8@users.noreply.github.com>
2023-08-16 17:55:41 +0300

    Add files via upload

--- main.php
+++ main.php
@@ -112,3 +112,40 @@ $number = 5;
 $result = factorial($number);
 echo "Factorial of $number is: " . $result;
 ?>
+
+<br>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+  <table width="400px" border="1px" cellspacing="0px">
+    <?php
+    $value = 0;
+    $col = 0;
+
+    do {
+      $row = 0;
+      echo "<tr>";
+      $value = $col;
+
+      do {
+        if ($value % 2 == 0) {
+          echo
+          "<td height=40px width=20px bgcolor=black></td>";
+          $value++;
+        } else {
+          echo
+          "<td height=40px width=20px bgcolor=white></td>";
+          $value++;
+        }
+        $row++;
+      } while ($row < 8);
+      echo "</tr>";
+      $col++;
+    } while ($col < 8);
+    ?>
+  </table>
+</body>
+
+</html>
